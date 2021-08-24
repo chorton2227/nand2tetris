25a295302aa3bb0a58077ffb2ba914f9522caed3
Chris Horton <chorton2227@gmail.com>
2021-08-24 12:40:56 -0400

    Remove .sh files

--- assembler/assemble.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-./assembler.py ../projects/06/add/Add.asm
-./assembler.py ../projects/06/max/Max.asm
-./assembler.py ../projects/06/max/MaxL.asm
-./assembler.py ../projects/06/pong/Pong.asm
-./assembler.py ../projects/06/pong/PongL.asm
-./assembler.py ../projects/06/rect/Rect.asm
-./assembler.py ../projects/06/rect/RectL.asm
