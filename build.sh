d610353677b83d3ac79876438d2a1b616849aabc
Chris Horton <chorton2227@gmail.com>
2021-09-04 21:10:17 -0400

    Update build to include os

--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@
 # Compile .jack to .vm
 ./compiler/compiler.py ./tetris/
 
+# Compile os and copy files to tetris
+./compiler/compiler.py ./os/
+cp ./os/out/* ./tetris/out/ -r
+
 # Translate .vm to .asm
 ./vm-translator/vm_translator.py ./tetris/out/
 
